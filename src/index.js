ce9562b34a97be053c3f593d47829f3f2b9319a9
Vilkas99 <victorium99@hotmail.com>
2020-10-17 20:04:35 -0500

    final

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider as Redux } from "react-redux";
+import store from "./app/store";
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Redux store={store}>
+      <App />
+    </Redux>
   </React.StrictMode>,
   document.getElementById('root')
 );
